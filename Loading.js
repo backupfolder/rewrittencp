7753a9a9c3b2f171ea98dccbaf8fbe9ecda3c462
bananedansunjeu <nadirzebiri2@gmail.com>
2021-08-14 20:20:44 -0400

    ajout du loading principal du jeu

--- Loading.js
+++ Loading.js
@@ -1,34 +1,16 @@
 /* global Phaser */
-let background, spinner, loadingcontent, loadingicon, loadingcontainer, loadingbar;
+let background, spinner, loadingcontent, loadingicon, loadingcontainer, loadingbar, progressBar;
+
 
 class Loading extends Phaser.Scene {
   constructor() {
     super('loadingGame');
   }
-  preload() {   
-    this.load.multiatlas('atlaspenguin-1', 'assets/json/atlaspenguin-1.json', 'assets');
-    this.load.multiatlas('atlaspenguin-0', 'assets/json/atlaspenguin-0.json', 'assets');
-    
-   /* this.load.on('progress', function (value) {
-    console.log(value);
-    progressBar.clear();
-    progressBar.fillStyle(0xffffff, 1);
-    progressBar.fillRect(250, 280, 300 * value, 30);
-    });
-    
-    this.load.on('fileprogress', function (file) {
-    console.log(file.src);
-    });
-    
-    this.load.on('complete', function () {
-    console.log('complete');
-    progressBar.destroy();
-    progressBox.destroy();
-    }); */
-   }
+  preload() {
+    /*loading */ this.load.multiatlas('atlaspenguin-1', 'assets/json/atlaspenguin-1.json', 'assets');
+    }
   
   create() {
-    
     background = this.add.image(640, 400, 'atlaspenguin-1', 'preload/blue_background');
     background.scale = 2;
         
@@ -39,11 +21,9 @@ class Loading extends Phaser.Scene {
     loadingcontainer.y = 350;
     
     loadingbar = this.add.graphics();
-    loadingbar.lineStyle(3, 0xFFFFFF, 1);
-    loadingbar.strokeRect(0, 0, 200, 32);
-    loadingbar.x = 1280/2 - 200/2;
-    loadingbar.y = 400;
-    
+    loadingbar.x = 1280 / 2 - 200 / 2 + 5;
+    loadingbar.y = 355;
+
     loadingcontent = this.add.text(0, 0, 'Loading Content', {fill: 'white', font: '33px Teko'});
     loadingcontent.x = 1280/2 - loadingcontent.width/2;
     loadingcontent.y = 400;
@@ -53,5 +33,19 @@ class Loading extends Phaser.Scene {
     loadingicon = this.add.sprite(0, 290, 'atlaspenguin-1', 'preload/spinner/1');
     loadingicon.x = 1280/2;
     loadingicon.play('animation');
+    
+    this.load.on('progress', function(value) {
+      console.log(value);
+      loadingbar.clear();
+      loadingbar.fillStyle(0xffffff, 1);
+      loadingbar.fillRect(0, 0, 190 * value, 22);
+    });
+    this.load.once('complete', () => {
+      console.log('chargement fini');
+      this.scene.start('betaGame');
+    });
+    
+    this.load.multiatlas('atlaspenguin-0', 'assets/json/atlaspenguin-0.json', 'assets');
+    this.load.start();
   }
 }
