868e14e4fef841890b220303c7140113a965f75e
bananedansunjeu <nadirzebiri2@gmail.com>
2021-08-15 16:00:04 -0400

    interactivié de btn: createpenguin & start

--- Welcome.js
+++ Welcome.js
@@ -1,4 +1,4 @@
-let unlock, createpenguin, cprlogo, whatsnew, welcomecontainer, start, background1, background2;
+let unlock, createpenguin, cprlogo, whatsnew, welcomecontainer, start, background1, checkout;
 
 class Welcome extends Phaser.Scene {
 
@@ -20,6 +20,19 @@ class Welcome extends Phaser.Scene {
     
     createpenguin = this.add.image(950, 610, 'atlaspenguin-0', 'core/startscreen/create_penguin');
     createpenguin.scale = 0.5;
+    createpenguin.setInteractive({ useHandCursor: true });
+    createpenguin.on('pointerdown', function() {
+      createpenguin.setTexture('atlaspenguin-0', 'core/startscreen/create_penguin2');
+    });
+    createpenguin.on('pointerover', function() {
+      createpenguin.setTexture('atlaspenguin-0', 'core/startscreen/create_penguin2');
+    }, this);
+    createpenguin.on('pointerup', function() {
+      createpenguin.setTexture('atlaspenguin-0', 'core/startscreen/create_penguin');
+    }, this);
+    createpenguin.on('pointerout', function() {
+      createpenguin.setTexture('atlaspenguin-0', 'core/startscreen/create_penguin');
+    });
     
     cprlogo = this.add.image(1280/2, 520, 'atlaspenguin-0', 'core/ui/cpr_logo');
     cprlogo.scale = 0.5;
@@ -27,8 +40,27 @@ class Welcome extends Phaser.Scene {
     whatsnew = this.add.image(330, 610, 'atlaspenguin-0', 'core/startscreen/whats_new');
     whatsnew.scale = 0.45;
     
-    start = this.add.image(640, 650, 'atlaspenguin-0', 'core/startscreen/start');
+    confirmation = this.add.sprite(640, 650, 'atlaspenguin-0', 'core/ui/button/blue_shaded/thin_1');
+    confirmation.scale = 0.8;
+    
+    confirmation.setInteractive({ useHandCursor: true });
+    confirmation.on('pointerdown', function() {
+      confirmation.setTexture('atlaspenguin-0', 'core/ui/button/blue_shaded/thin_2');
+    });
+    confirmation.on('pointerover', function() {
+      confirmation.setTexture('atlaspenguin-0', 'core/ui/button/blue_shaded/thin_2');
+    }, this);
+    confirmation.on('pointerup', function() {
+      confirmation.setTexture('atlaspenguin-0', 'core/ui/button/blue_shaded/thin_1');
+      this.scene.start('mineExterieur');
+    }, this);
+    confirmation.on('pointerout', function() {
+      confirmation.setTexture('atlaspenguin-0', 'core/ui/button/blue_shaded/thin_1');
+    });
 
+    start = this.add.image(640, 650, 'atlaspenguin-0', 'core/startscreen/start');
+    
+    checkout = this.add.text(210, 660, "CHECK OUT WHAT'S NEW!", { fill: 'white', font: '30px Teko' });
   }
 
 }
