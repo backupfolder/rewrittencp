777b89910b58f5bda6f8d6915c98006d37782ae3
bananedansunjeu <nadirzebiri2@gmail.com>
2021-08-16 07:06:40 -0400

    ajout de sprites sur l'interface

--- Interface.js
+++ Interface.js
@@ -1,4 +1,4 @@
-let mailIcon, mapIcon, penguindance, thing;
+let mailIcon, mapIcon, penguindance, mod, new1, news, dock, dockText, thing;
 class Interface extends Phaser.Scene {
 
     constructor() {
@@ -10,8 +10,8 @@ class Interface extends Phaser.Scene {
     }
     
     create() {
-      mailIcon = this.add.sprite(200, 100, 'matlasInterface', 'room_basic/tools/mail/mail1');
-      mapIcon = this.add.sprite(80, 650, 'matlasInterface', 'room_basic/tools/map/map1');
+      mapIcon = this.add.sprite(75, 650, 'matlasInterface', 'room_basic/tools/map/map1');
+      mapIcon.scale = 0.85;
       
       mapIcon.setInteractive({ useHandCursor: true });
     mapIcon.on('pointerdown', function() {
@@ -29,6 +29,61 @@ class Interface extends Phaser.Scene {
     
     
       penguindance = this.add.sprite(500, 700, 'matlasInterface', 'room_basic/dock/actions/penguin_dance');
-      thing = this.add.sprite(500, 800, 'matlasInterface', 'room_basic/tools/epf/epf2');
+      mod = this.add.sprite(1210, 60, 'matlasInterface', 'room_basic/tools/mod/mod1');
+      mod.scale = 0.95;
+      
+      mod.setInteractive({ useHandCursor: true });
+      mod.on('pointerdown', function() {
+        mod.setTexture('matlasInterface', 'room_basic/tools/mod/mod2');
+      });
+      mod.on('pointerover', function() {
+        mod.setTexture('matlasInterface', 'room_basic/tools/mod/mod2');
+      }, this);
+      mod.on('pointerup', function() {
+        mod.setTexture('matlasInterface', 'room_basic/tools/mod/mod1');
+      }, this);
+      mod.on('pointerout', function() {
+        mod.setTexture('matlasInterface', 'room_basic/tools/mod/mod1');
+      });
+      
+      mailIcon = this.add.sprite(150, 50, 'matlasInterface', 'room_basic/tools/mail/mail1');
+      mailIcon.scale = 0.85;
+      
+      mailIcon.setInteractive({ useHandCursor: true });
+      mailIcon.on('pointerdown', function() {
+        mailIcon.setTexture('matlasInterface', 'room_basic/tools/mail/mail2');
+      });
+      mailIcon.on('pointerover', function() {
+        mailIcon.setTexture('matlasInterface', 'room_basic/tools/mail/mail2');
+      }, this);
+      mailIcon.on('pointerup', function() {
+        mailIcon.setTexture('matlasInterface', 'room_basic/tools/mail/mail1');
+      }, this);
+      mailIcon.on('pointerout', function() {
+        mailIcon.setTexture('matlasInterface', 'room_basic/tools/mail/mail1');
+      });
+      
+      news = this.add.sprite(60, 60, 'matlasInterface', 'room_basic/tools/news/news1');
+      news.setInteractive({ useHandCursor: true });
+      news.on('pointerdown', function() {
+        news.setTexture('matlasInterface', 'room_basic/tools/news/news2');
+      });
+      news.on('pointerover', function() {
+        news.setTexture('matlasInterface', 'room_basic/tools/news/news2');
+      }, this);
+      news.on('pointerup', function() {
+        news.setTexture('matlasInterface', 'room_basic/tools/news/news1');
+      }, this);
+      news.on('pointerout', function() {
+        news.setTexture('matlasInterface', 'room_basic/tools/news/news1');
+      });
+      new1 = this.add.sprite(85, 90, 'matlasInterface', 'room_basic/tools/news/new0001');
+      news.scale = 0.95;
+      new1.scale = 0.95;
+      
+      dock = this.add.sprite(1280/2, 685, 'matlasInterface', 'room_basic/dock/dock');
+      dockText = this.add.sprite(1280/2, 695, 'matlasInterface', 'room_basic/dock/dock_text');
+      dockText.scale = 0.5;
+      // icones room_basic/dock/icons/dock_home
     }
 }
